bd5bc21b1144eeb33ebad232478bd3c4f4ddd8fc
Lyubov Agulova <lu7623@gmail.com>
2023-11-07 15:05:03 +0300

    feat: install Jest test

---
+++ src/routes/components/Loading.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/react';
+import Loading from './Loading';
+
+describe('Loading page', () => {
+  it('renders loader', () => {
+    render(<Loading />);
+
+    const loader = screen.getByAltText('loading');
+
+    expect(loader).toBeInTheDocument();
+  });
+});
