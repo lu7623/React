2f111e68c741d43343c0b7c21297bc4c5db4a634
Lyubov Agulova <lu7623@gmail.com>
2023-11-27 21:26:18 +0300

    fix: some fix

--- src/pages/page/[pageId].tsx
+++ src/pages/page/[pageId].tsx
@@ -20,12 +20,12 @@ export const getServerSideProps = wrapper.getServerSideProps(
     const listId = parseInt(context.params?.pageId as string) || 1;
     const qty = parseInt(context.query?.qty as string) || 20;
     let pokemons: Pokemon[] = [];
-
-    const newPokemons = await store.dispatch(
-      getPokemonsByPage.initiate({ pageNum: listId, qty: qty })
-    );
-    pokemons = newPokemons.data ? newPokemons.data : [];
-
+    if (typeof listId === 'number' && typeof qty === 'number') {
+      const newPokemons = await store.dispatch(
+        getPokemonsByPage.initiate({ pageNum: listId, qty: qty })
+      );
+      pokemons = newPokemons.data ? newPokemons.data : [];
+    }
     await Promise.all(store.dispatch(getRunningQueriesThunk()));
     return {
       props: {
