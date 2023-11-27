02abde77c8103355e4343cc222c5a2d0de57853f
Lyubov Agulova <lu7623@gmail.com>
2023-11-27 20:23:14 +0300

    fix: some fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,11 @@
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
+
 export default function Page() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push('/page/1?qty=20');
+  }, []);
+
   return <></>;
 }
