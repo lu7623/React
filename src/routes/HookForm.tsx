c3cfa7c1dfab6376808d626ae761149b8ffdda58
Lyubov Agulova <lu7623@gmail.com>
2023-12-03 00:04:52 +0300

    fix: fix select country

--- src/routes/HookForm.tsx
+++ src/routes/HookForm.tsx
@@ -1,66 +1,137 @@
-import { useForm, SubmitHandler } from 'react-hook-form';
-import { Countries } from 'utils/types';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
-export enum GenderEnum {
-  female = 'female',
-  male = 'male',
-  other = 'other',
-}
-
-export interface IFormInput {
+interface IFormInput {
   name: string;
   age: number;
   email: string;
   password: string;
-  gender: GenderEnum;
-  country: Countries;
-  acceptTerms: boolean;
-  picture: File;
+  confirm: string;
+  gender: string;
+  //country: string;
+  accept: NonNullable<boolean | undefined>;
 }
 
+const getCharacterValidationError = (str: string) => {
+  return `Your password must have at least 1 ${str} character`;
+};
+const schema = yup.object().shape({
+  name: yup
+    .string()
+    .required('Please enter your name')
+    .uppercase('Name should be uppercase'),
+  age: yup
+    .number()
+    .positive('Age is not valid')
+    .integer('Age is not valid')
+    .required('Please enter your age'),
+  email: yup
+    .string()
+    .email('E-mail is not valid')
+    .required('Please enter your email'),
+  password: yup
+    .string()
+    .required()
+    .matches(/[0-9]/, getCharacterValidationError('digit'))
+    .matches(/[a-z]/, getCharacterValidationError('lowercase'))
+    .matches(/[A-Z]/, getCharacterValidationError('special charcter'))
+    .matches(/[\W|_/g]/, getCharacterValidationError('special caracter')),
+  confirm: yup
+    .string()
+    .required('Please re-type your password')
+    .oneOf([yup.ref('password')], 'Passwords does not match'),
+  gender: yup
+    .string()
+    .required('Please select your gender')
+    .oneOf(['male', 'female', 'other']),
+  // country: yup.string().oneOf(countriesArr).required('Please select your country'),
+  accept: yup
+    .bool()
+    .required('You have to accept terms and conditions to coninue')
+    .oneOf([true], 'You must accept the terms and conditions'),
+});
+
 export default function HookForm() {
-  const { register, handleSubmit } = useForm<IFormInput>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormInput>({
+    resolver: yupResolver(schema),
+  });
   const onSubmit: SubmitHandler<IFormInput> = (data) => {
-    console.log(data);
-    console.log('file img' + returnFileSize(data.picture.size));
+    console.log({ data });
   };
-  function returnFileSize(number: number) {
-    if (number < 1024) {
-      return `${number} bytes`;
-    } else if (number >= 1024 && number < 1048576) {
-      return `${(number / 1024).toFixed(1)} KB`;
-    } else if (number >= 1048576) {
-      return `${(number / 1048576).toFixed(1)} MB`;
-    }
-  }
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>Name:</label>
-      <input {...register('name')} />
-      <label>Age:</label>
-      <input {...register('age')} />
-      <label>Email:</label>
-      <input {...register('email')} />
-      <label>Password:</label>
-      <input {...register('password')} />
-      <label>Confirm password:</label>
-      <input {...register('password')} />
-      <label>Gender Selection</label>
-      <select {...register('gender')}>
-        <option value="female">female</option>
-        <option value="male">male</option>
-        <option value="other">other</option>
-      </select>
-      <input type="checkbox" id="acceptTerms" {...register('acceptTerms')} />
-      <label htmlFor="acceptTerms">Accept terms and conditions</label>
-      <label htmlFor="picture">Choose image to upload (PNG, JPG)</label>
-      <input
-        type="file"
-        id="picture"
-        {...register('picture')}
-        accept="image/png, image/jpeg, image/jpg"
-      />
-      <input type="submit" />
-    </form>
+    <>
+      {' '}
+      <div className="flex justify-center items-center flex-col w-screen">
+        <h2 className=" text-2xl">React hook form</h2>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+          <label>Name:</label>
+          <input {...register('name')} className=" bg-slate-200" />
+          {errors.name && (
+            <p className=" text-red-700 text-xs">{errors.name.message}</p>
+          )}
+
+          <label>Age:</label>
+          <input {...register('age')} className=" bg-slate-200" />
+          {errors.age && (
+            <p className=" text-red-700 text-xs">{errors.age.message}</p>
+          )}
+
+          <label>Email:</label>
+          <input {...register('email')} className=" bg-slate-200" />
+          {errors.email && (
+            <p className=" text-red-700 text-xs">{errors.email.message}</p>
+          )}
+
+          <label>Password:</label>
+          <input {...register('password')} className=" bg-slate-200" />
+          {errors.password && (
+            <p className=" text-red-700 text-xs">{errors.password.message}</p>
+          )}
+
+          <label>Confirm password:</label>
+          <input {...register('confirm')} className=" bg-slate-200" />
+          {errors.confirm && (
+            <p className=" text-red-700 text-xs">{errors.confirm.message}</p>
+          )}
+
+          <label>Gender Selection</label>
+          <select {...register('gender')}>
+            <option value="female" className=" bg-slate-200">
+              female
+            </option>
+            <option value="male" className=" bg-slate-200">
+              male
+            </option>
+            <option value="other" className=" bg-slate-200">
+              other
+            </option>
+          </select>
+          {errors.gender && (
+            <p className=" text-red-700 text-xs">{errors.gender.message}</p>
+          )}
+
+          <div className="flex">
+            <label htmlFor="acceptTerms">Accept terms and conditions</label>
+            <input type="checkbox" id="acceptTerms" {...register('accept')} />
+            {errors.accept && (
+              <p className=" text-red-700 text-xs">{errors.accept.message}</p>
+            )}
+          </div>
+
+          <input
+            type="submit"
+            className=" disabled:bg-gray-500 bg-green-900 text-white"
+            value="Submit"
+          />
+        </form>
+      </div>
+    </>
   );
 }
--- src/routes/Uncontrolled.tsx
+++ src/routes/Uncontrolled.tsx
@@ -1,10 +1,10 @@
-import React, { useRef } from 'react';
+import { useRef } from 'react';
 
 export default function Uncontrolled() {
-  const inputRef = useRef<HTMLInputElement>(null);
+  const nameInputRef = useRef<HTMLInputElement>(null);
 
   function handleSubmit() {
-    alert(`Name: ${inputRef.current ? inputRef.current.value : ''}`);
+    alert(`Name: ${nameInputRef.current ? nameInputRef.current.value : ''}`);
   }
 
   return (
@@ -13,7 +13,34 @@ export default function Uncontrolled() {
       <h3>Uncontrolled Component</h3>
       <form onSubmit={handleSubmit}>
         <label>Name :</label>
-        <input type="text" name="name" ref={inputRef} />
+        <input type="text" name="name" ref={nameInputRef} />
+        <button type="submit">Submit</button>
+      </form>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name:</label>
+        <input type="text" name="name" id="name" />
+        <label htmlFor="age">Age:</label>
+        <input type="text" name="age" id="age" />
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <label htmlFor="confirm">Confirm password:</label>
+        <input type="password" id="confirm" name="confirm" />
+        <label htmlFor="gender">Gender Selection</label>
+        <select id="gender">
+          <option value="female">female</option>
+          <option value="male">male</option>
+          <option value="other">other</option>
+        </select>
+        <input type="checkbox" id="acceptTerms" name="acceptTerms" />
+        <label htmlFor="acceptTerms">Accept terms and conditions</label>
+        <label htmlFor="picture">Choose image to upload (PNG, JPG)</label>
+        <input
+          type="file"
+          id="picture"
+          accept="image/png, image/jpeg, image/jpg"
+        />
         <button type="submit">Submit</button>
       </form>
     </div>
